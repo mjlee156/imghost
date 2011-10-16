7940ee0f78055547fd5c2e8863b313d61733ccf7
ryan <ryan@ryands.org>
2011-10-16 01:16:11 -0400

    Cleaned up the POST code to be more generic (no longer repeats two blocks of very similar code)

--- image/views.py
+++ image/views.py
@@ -19,78 +19,53 @@ def upload(request):
     if request.method == 'GET':
         return render_to_response('upload.html', {'url': request.GET.get('url', ''),}, context_instance=RequestContext(request))
     elif request.method == 'POST':
+        tmp = tempfile.mkstemp()
+        md5 = hashlib.md5()
+        fext = ""
+        orig = ""
+        
         if request.POST['upload_type'] == 'file':
             file = request.FILES['upload_file']
-            img = Image()
-            
-            try:
-                next_id = Image.objects.order_by('-id')[0].id + 1
-            except IndexError:
-                next_id = settings.IMAGE_ID_OFFSET + 1
-            
-            img.base62 = base62(next_id)
-            img.filename = base62(next_id) + "." + file.name[-3:]
-            img.orig_filename = file.name
-            img.type = file.content_type
-            img.description = '' # not implemented yet.
-            img.uploader = request.user
-
-            image_file = os.path.join(settings.MEDIA_ROOT,img.filename)
-            thumbnail = os.path.join(settings.MEDIA_ROOT, 'thumbs', img.filename)
-            
-            md5 = hashlib.md5()
-            tmp = tempfile.mkstemp()
+            fext = file.name[-3:]
+            orig = file.name
             f = os.fdopen(tmp[0], "wb+")
             for chunk in file.chunks():
                 f.write(chunk)
                 md5.update(chunk)
             f.close()
-            img.md5sum = md5.hexdigest()
-
-            shutil.move(tmp[1], image_file)
-            os.system("/usr/bin/convert %s -thumbnail 150x150 %s" % (image_file, thumbnail))
-            img.save()
-
-
-            return HttpResponseRedirect('/i/' + img.base62)
 
         elif request.POST['upload_type'] == 'url':
             remote_image = urllib2.urlopen(request.POST['upload_url'])
-            md5 = hashlib.md5()
-            
             data = remote_image.read()
             md5.update(data)
-
-            img = Image()
-            try:
-                next_id = Image.objects.order_by('-id')[0].id + 1
-            except IndexError:
-                next_id = settings.IMAGE_ID_OFFSET + 1
+            fext = request.POST['upload_url'][-3:]
+            orig = request.POST['upload_url']
             
-            img.base62 = base62(next_id)
-            img.filename = base62(next_id) + "." + request.POST['upload_url'][-3:]
-            img.orig_filename = request.POST['upload_url']
-            img.type = ''
-            img.description = '' # not implemented yet.
-            img.uploader = request.user
-            img.md5sum = md5.hexdigest()
-
-            image_file = os.path.join(settings.MEDIA_ROOT,img.filename)
-            thumbnail = os.path.join(settings.MEDIA_ROOT, 'thumbs', img.filename)
-            
-            #f = open(image_file, "wb+")
-            #f.write(data)
-            #f.close()
-            tmp = tempfile.mkstemp()
             f = os.fdopen(tmp[0], "wb+")
             f.write(data)
             f.close()
-            
-            shutil.move(tmp[1], image_file)
-            os.system("/usr/bin/convert %s -thumbnail 150x150 %s" % (image_file, thumbnail))
-            img.save()
 
-            return HttpResponseRedirect('/i/' + img.base62)
+        img = Image()
+        try:
+            next_id = Image.objects.order_by('-id')[0].id + 1
+        except IndexError:
+            next_id = settings.IMAGE_ID_OFFSET + 1
+        
+        img.base62 = base62(next_id)
+        img.filename = base62(next_id) + "." + fext
+        img.orig_filename = orig
+        img.type = '' # todo
+        img.description = '' # not implemented yet.
+        img.uploader = request.user
+        img.md5sum = md5.hexdigest()
+        image_file = os.path.join(settings.MEDIA_ROOT,img.filename)
+        thumbnail = os.path.join(settings.MEDIA_ROOT, 'thumbs', img.filename)
+            
+        shutil.move(tmp[1], image_file)
+        os.system("/usr/bin/convert %s -thumbnail 150x150 %s" % (image_file, thumbnail))
+        img.save()
+
+        return HttpResponseRedirect('/' + img.filename)
 
 @login_required
 def view_image(request, id):
